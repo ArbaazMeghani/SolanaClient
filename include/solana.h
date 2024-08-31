e65a9b3638217a9c71e999b0aa88b6caec283d2d
Arbaaz Meghani <arbaazmeghani@gmail.com>
2024-08-31 17:50:49 -0500

    cleanup

--- include/solana.h
+++ include/solana.h
@@ -15,21 +15,8 @@ public:
     ~Solana();
     std::string getRpcUrl();
     uint64_t getBalance(const std::string &publicKey);
-    // std::string transfer(const std::string &from, const std::string &to, const std::string &amount);
-    // std::string createAccount();
-    // std::string createToken(const std::string &mintAuthority, const std::string &freezeAuthority);
-    // std::string mintToken(const std::string &mintAuthority, const std::string &to, const std::string &amount);
-    // std::string transferToken(const std::string &from, const std::string &to, const std::string &amount);
     Account getAccountInfo(const std::string &publicKey);
-    // std::string getTokenAccountInfo(const std::string &publicKey);
-    // std::string getMintInfo(const std::string &publicKey);
     std::string_view getLatestBlockhash();
-    // std::string createAndTransfer(const std::string &from, const std::string &to, const std::string &amount);
-    // std::string createAndTransferToken(const std::string &from, const std::string &to, const std::string &amount);
-    // std::string createAndTransferTokenWithAuthority(const std::string &from, const std::string &to, const std::string &amount, const std::string &authority);
-    // std::string createAndTransferTokenWithAuthorityAndFreezeAuthority(const std::string &from, const std::string &to, const std::string &amount, const std::string &authority, const std::string &freezeAuthority);
-    // std::string createAndTransferTokenWithAuthorityAndFreezeAuthorityAndMintAuthority(const std::string &from, const std::string &to, const std::string &amount, const std::string &authority, const std::string &freezeAuthority, const std::string &mintAuthority);
-    // std::string createAndTransferTokenWithAuthorityAndFreezeAuthorityAndMintAuthorityAndToken(const std::string &from, const std::string &to, const std::string &amount, const std::string &authority, const std::string &freezeAuthority, const std::string &mintAuthority, const std::string &token = "");
 
 private:
     CURL *curl_;
